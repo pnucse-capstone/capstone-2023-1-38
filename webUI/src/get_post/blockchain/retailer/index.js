90e4e48ecb16da162789621735791d44397bf41a
sy12340 <92321623+sy12340@users.noreply.github.com>
2023-10-23 20:53:40 +0900

    Delete webUI directory

--- webUI/src/get_post/blockchain/retailer/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function updateReceiveStatus(data) {
-  return request({
-    url: '/traces/retailer/updateReceiveStatus',
-    method: 'post',
-	data:data
-  })
-}
--- webUI/src/get_post/menu.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 라우터 가져오기
-export const getRouters = () => {
-  return request({
-    url: '/getRouters',
-    method: 'get'
-  })
-}
